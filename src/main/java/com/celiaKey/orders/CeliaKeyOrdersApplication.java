a36448d0bb77a80aa955a58a990c0807109b5d22
tans <1170138823@qq.com>
2023-05-20 07:14:57 +0800

    启动打印日志

--- src/main/java/com/celiaKey/orders/CeliaKeyOrdersApplication.java
+++ src/main/java/com/celiaKey/orders/CeliaKeyOrdersApplication.java
@@ -1,13 +1,16 @@
 package com.celiaKey.orders;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class CeliaKeyOrdersApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CeliaKeyOrdersApplication.class, args);
+		log.info("------------- orders 启动成功 ----------------");
 	}
 
 }
